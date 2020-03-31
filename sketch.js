0fe7cf481931928a1b8e2a498eaa3e78c107ffcf
indiramamidala <62195277+indiramamidala@users.noreply.github.com>
2020-03-31 17:44:36 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,80 @@
+
+// CREATE GLOBAL VARIABLES
+// For Engine, World, Bodies and any other that you have in mind to make your coding life easier.
+// remember to create an array of boxes.
+
+var Engine = Matter.Engine,
+    World = Matter.World,
+    Bodies = Matter.Bodies;
+ 
+var engine;
+var world;
+var boxes = [];
+ 
+var ground;
+var gSlider;
+ 
+function setup() {
+    createCanvas(400, 400);
+
+    // Create an instance of Engine, World
+    engine = Engine.create();
+    world = engine.world;
+ 
+    // A slider is already created for you here. This slider will dictate the gravity of the world
+    gSlider = createSlider(0, 100, 50);
+    gSlider.position(40, 365);
+    gSlider.input = map(engine.world.gravity, gSlider.min, gSlider.max, 0, 1);
+ 
+    // Create a ground rectangle that would hold all the boxes and add it to the world
+    ground = new Ground(400,400);
+    world.add(world.ground);
+}
+    console.log(box.position.x);
+    console.log(box.position.y);
+function mousePressed() {
+    if (mouseY < 350) {
+        // Every time a mouse press occures create a new box.
+        box = new Box(200,200,10,20);
+       
+    }
+}
+ 
+function draw() {
+    // Draw all the elements including the slider that 
+
+    background(51);
+    // This is the value of your gravity. You can optionally show it to the viewer.
+    var fVal = gSlider.value();
+ 
+    
+    ellipse(box.position.x,box.position.y,10,10);
+    rect(ground.position.x,ground.y,50,50);
+ 
+
+    // Use a for loop to show all the boxes
+
+}
+ 
+
+// You can either create a file for the class Box or build a simple function that creates one box at a time.
+// I have gone for the second option.
+function Box(x, y, w, h, options) {
+
+    // add options such as friction and restitution. Experiment with the values
+    var options = {
+friction = 3,
+restitution = 4,
+    }
+ 
+    // create your box using the function arguments
+    // x - x-coordinate
+    // y - y-coordinate
+    // w - width of the box
+    // h - height of the box
+
+    // Create a show method which will draw the box every time it is called inside the draw method.
+    // remember to push and pop.
+    this.show = function () {
+    }
+}
